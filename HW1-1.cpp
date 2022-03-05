a176199771d029688eed8af46a915b8d0e9e0c26
minmin-pan <79898769+minmin-pan@users.noreply.github.com>
2022-03-05 23:08:03 +0800

    Update HW1-1.cpp

--- HW1-1.cpp
+++ HW1-1.cpp
@@ -1,8 +1,8 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
@@ -33,18 +33,20 @@ int main()
 {
     int i;
     int num;
-    cout << "type data number:";
-    cin >> num;
-    
-    int prices2[num];
-    for (i = 0; i < num; i++){
-        cout << "price:";
-        cin >> prices2[i];
-    }
-    for (i = 0; i < num; i++)
-        printf("prices[%d] = %d\n", i, prices2[i]);
+    cout << "type how many day stock data(if want to end please input 'end'):";
+    while (cin >> num){
+        //cin >> num;
         
-    printf("Output: %d",maxProfit(prices2, num));
-
+        int prices2[num];
+        for (i = 0; i < num; i++){
+            cout << "price:";
+            cin >> prices2[i];
+        }
+        for (i = 0; i < num; i++)
+            printf("prices[%d] = %d\n", i, prices2[i]);
+            
+        printf("Output: %d\n",maxProfit(prices2, num));
+        cout << "type how many day stock data(if want to end please input 'end'):";
+    }
     return 0;
 }
